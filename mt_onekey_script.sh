013ad71c24b251c3132ff714ac7945b696e74a25
kenshinyelin <kenshinyelin@gmail.com>
2019-07-30 17:50:36 +0800

    Update mt_onekey_script.sh
    add param

--- mt_onekey_script.sh
+++ mt_onekey_script.sh
@@ -7,9 +7,9 @@ npm i pm2 -g;
 ln -s /root/node-v10.16.0-linux-x64/bin/pm2 /usr/local/bin/pm2;
 sudo apt-get update;
 sudo apt-get install expect -y;
-apt install git
+apt install git;
 git clone https://gitee.com/Eric_HSBC/mt_update_bynodejs.git;
 cp /root/mt_update_bynodejs/expect.sh  /root/expect.sh && chmod +x expect.sh
 cd mt_update_bynodejs && npm i;
 pm2 start bin/www -- name mt;
-wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/mtproxy_go.sh && chmod +x mtproxy_go.sh 
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/mtproxy_go.sh && chmod +x mtproxy_go.sh;
